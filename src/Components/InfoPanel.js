4ce07f1e623c0b05b9705eda9c51ad63a659c212
José Roberto Chávez Cobián <rrc0138@gmail.com>
2018-09-06 00:12:53 -0500

    up

--- src/Components/InfoPanel.js
+++ src/Components/InfoPanel.js
@@ -1,21 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Transition, CSSTransition } from 'react-transition-group'
 import styles from './InfoPanel.scss';
 import image from '../assets/img/me.jpg';
 
-console.log(styles)
-
 class InfoPanel extends Component {
+  state = {
+    isImgBlurry : true
+  }
+
   render() {
+    const {isImgBlurry} = this.state;
+
     return (
       <section className={`${styles.infoSide}`}>
         <div className={`${styles.top}`}>
-          <img src={image}></img>
+          <div className={`${styles.image}`}>
+            <img src={image} className={isImgBlurry ? styles.blurry : ''}></img>
+          </div>
         </div>
         <div className={`${styles.info}`}>
-          {this.props.children}
+          Hello World
         </div>
       </section>
     )
+  }
+
+  componentDidMount() {
+    this.setState({
+      isImgBlurry : false
+    });
   }
 }
 
