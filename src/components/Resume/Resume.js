d0a21d1e6cfe1f4137472a1a9e417b840b75126a
José Roberto Chávez Cobián <rrc0138@gmail.com>
2020-04-28 20:12:03 -0500

    Update package-lock

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -42,7 +42,7 @@ const Resume = () => {
             <div className={column}>
               <Career company="Accedo" period="Octuber 2018 - currently" title="Web Engineer">
                 Working on multiple web & smart TV apps, focused on streaming, and using technologies such as
-                Angular, React, or plain JS.
+                Angular, React, and plain JS.
               </Career>
             </div>
           </div>
